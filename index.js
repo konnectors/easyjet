8f8a058a67a539eef2bdbe101eb8802d3d4c945f
doubleface <christophe@cozycloud.cc>
2018-05-14 09:29:25 +0200

    publish: Update dependency cozy-konnector-libs to v4.2.0
    generated from commit f93f2b24a5ce8285bdfd930d913f945e644ee2a8

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-const https = require('https')
-const baseUrl = 'https://www.easyjet.com'
-
-console.log('begin')
-https
-  .get(`${baseUrl}/fr`, resp => {
-    let data = ''
-
-    // A chunk of data has been recieved.
-    resp.on('data', chunk => {
-      // console.log('data')
-      data += chunk
-    })
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log('end')
-      // console.log(data, 'data')
-    })
-  })
-  .on('error', err => {
-    console.log('Error: ' + err.message)
-  })
+console.log('before')
+require('https').get('https://www.easyjet.com/fr')
+console.log('after')
